397ad0f85d63c916691589bf377426c74e4aac47
dapliyan1100 <145763255+dapliyan1100@users.noreply.github.com>
2023-11-09 21:54:27 +0800

    Button progress

--- Trip2600-Byahe2600/app/src/main/java/com/example/trip2600_byahe2600/LoginActivity.kt
+++ Trip2600-Byahe2600/app/src/main/java/com/example/trip2600_byahe2600/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.trip2600_byahe2600
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val buttonClick = findViewById<Button>(R.id.button3)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
